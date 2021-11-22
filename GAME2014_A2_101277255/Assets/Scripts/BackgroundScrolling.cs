4e68e6592c6b3aa99ee531c9a9e1e2e4a9573d76
RuiGeng <ruichengeng@gmail.com>
2021-11-22 06:10:13 -0500

    changed the layout of the ui and added new scenes instead of changing the panels

--- GAME2014_A2_101277255/Assets/Scripts/BackgroundScrolling.cs
+++ GAME2014_A2_101277255/Assets/Scripts/BackgroundScrolling.cs
@@ -1,3 +1,11 @@
+/*
+ Full Name: Rui Chen Geng Li (101277255)
+ File Name: BackgroundScrolling.cs
+ Last Modified: November 21st, 2021
+ Description: Scrolling the scroll of cloud background
+ Version History: v1.01 Initial blank script and internal documentation
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- GAME2014_A2_101277255/Assets/Scripts/PlayerScript.cs
+++ GAME2014_A2_101277255/Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,11 @@
+/*
+ Full Name: Rui Chen Geng Li (101277255)
+ File Name: PlayerScript.cs
+ Last Modified: November 21st, 2021
+ Description: Defines the player's behaviour
+ Version History: v1.01 Initial blank script and internal documentation
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
